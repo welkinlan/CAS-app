16181995e54ed8330841d6a11e4d50c7ad2eb67c
Tian Lan <welkinlan@gmail.com>
2015-07-01 14:34:00 -0500

    fix upload bug

--- src/com/cas/server/BackgroundSync.java
+++ src/com/cas/server/BackgroundSync.java
@@ -111,31 +111,30 @@ public class BackgroundSync {
 		files = fileDir.listFiles();
 		if (files == null) {
 			Log.i(UTAG, "no directory");
-			return;
 		}
-		if (files.length == 0) {
+		else if (files.length == 0) {
 			Log.i(UTAG, "no files");
-			return;
 		}
-		app.isUploading = true;
-		for (File file : files) {
-			HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(ServerUrl.usage_upload_url);
-			MultipartEntity multipartEntity = new MultipartEntity();
-			//upload with http
-			try {
-				multipartEntity.addPart("file", new FileBody(file));
-				//set entities
-				post.setEntity(multipartEntity);
-				//get responses
-				HttpResponse resp = client.execute(post);
-				file.delete();
-				Log.i(UTAG, ServerUrl.usage_upload_url + ": " + resp.getStatusLine() + "");
-			} catch (Exception e) {
-				Log.e(UTAG, "Error" + e.getMessage());
-				e.printStackTrace();
-				app.isUploading = false;
-				return;
+		else {
+			app.isUploading = true;
+			for (File file : files) {
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost(ServerUrl.usage_upload_url);
+				MultipartEntity multipartEntity = new MultipartEntity();
+				//upload with http
+				try {
+					multipartEntity.addPart("file", new FileBody(file));
+					//set entities
+					post.setEntity(multipartEntity);
+					//get responses
+					HttpResponse resp = client.execute(post);
+					file.delete();
+					Log.i(UTAG, ServerUrl.usage_upload_url + ": " + resp.getStatusLine() + "");
+				} catch (Exception e) {
+					Log.e(UTAG, "Error" + e.getMessage());
+					e.printStackTrace();
+					app.isUploading = false;
+				}
 			}
 		}
 		
@@ -147,130 +146,67 @@ public class BackgroundSync {
 		files = fileDir.listFiles();
 		if (files == null) {
 			Log.i(UTAG, "no directory");
-			return;
 		}
-		if (files.length == 0) {
+		else if (files.length == 0) {
 			Log.i(UTAG, "no files");
-			return;
 		}
-		app.isUploading = true;
-		for (File file : files) {
-			String file_username = file.getName().split("_")[0];
-			// Log.i(UTAG,file_username );
-			saved = context.getSharedPreferences(file_username, MODE_PRIVATE);
-			String recording_completed = saved.getString(file.getName()+"completed","0");
-			if(recording_completed.equals("0")){
-				Log.i(UTAG, file_username+" is still recording this file.");
-				continue;
-			}
-			String upload_recording_url = saved.getString(file.getName(),"");
-			// Log.i(UTAG,upload_url );
-			String[] contents = sh.uploadUrlHelper(upload_recording_url);
-			HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(ServerUrl.audio_upload_url);
-			MultipartEntity multipartEntity = new MultipartEntity();
-			//upload with http
-			try {
-				multipartEntity.addPart("contextid",
-						new StringBody(contents[0]));
-				multipartEntity.addPart("component",
-						new StringBody(contents[1]));
-				multipartEntity
-						.addPart("filearea", new StringBody(contents[2]));
-				multipartEntity
-						.addPart("filename", new StringBody(contents[3]));
-				
-				String star = saved.getString(file.getName()+"star","none");
-				multipartEntity
-			    		.addPart("star", new StringBody(star));
-				
-				String mode = saved.getString(file.getName()+"mode","none");
-				multipartEntity
-			    		.addPart("mode", new StringBody(mode));
-				multipartEntity.addPart("file", new FileBody(file));
-
-				post.setEntity(multipartEntity);
-				HttpResponse resp = client.execute(post);
-				Log.i(UTAG, upload_recording_url + ": " + resp.getStatusLine()
-						+ "");
-				
-				moveFile(file.getName());
-				// if (resp.getStatusLine().getStatusCode() == 200) {
-				// Log.i("File Upload","Success");
-				file.delete();
-				// }
-
-			} catch (Exception e) {
-				Log.e(UTAG, "Error" + e.getMessage());
-				e.printStackTrace();
-				app.isUploading = false;
-				return;
-			}
-		}
-		app.isUploading = false;
-	}
-	// Upload section with HttpUrlConnection
-	/*
-
-	public void upload() {
-		// start upload
-		File fileDir = new File(Environment.getExternalStorageDirectory()
-				+ "/CAS_Audio/");
-		files = fileDir.listFiles();
-		HttpURLConnection up_conn = null;
-		try {
+		else {
+			app.isUploading = true;
 			for (File file : files) {
-				String upload_url = saved.getString(file.getName(), "");
-				up_conn = (HttpURLConnection) new URL(upload_url).openConnection();
-				up_conn.setConnectTimeout(30000);
-				up_conn.setReadTimeout(30000);
-				String end = "\r\n";
-				String hyphens = "--";
-				String boundary = "*****";
-				up_conn.setChunkedStreamingMode(128 * 1024);
-				up_conn.setDoInput(true);
-				up_conn.setDoOutput(true);
-				up_conn.setUseCaches(false);
-				up_conn.setRequestMethod("POST");
-				up_conn.setRequestProperty("Charset", "UTF-8");
-				up_conn.setRequestProperty("up_connection", "Keep-Alive");
-				up_conn.setRequestProperty("Content-Type",
-						"multipart/form-data;boundary=" + boundary);
-				Log.i(UTAG + "begin upload", file.getName());
-				DataOutputStream ds = new DataOutputStream(
-						up_conn.getOutputStream());
-				ds.writeBytes(hyphens + boundary + end);
-				ds.writeBytes("Content-Disposition: form-data; "
-						+ "name=\"file\";filename=\"" + file.getName() + "\""
-						+ end);
-				ds.writeBytes(end);
-				FileInputStream input = new FileInputStream(file);
-				int size = 1024;
-				byte[] buffer = new byte[size];
-				int length = -1;
-				while ((length = input.read(buffer)) != -1) {
-					ds.write(buffer, 0, length);
+				String file_username = file.getName().split("_")[0];
+				// Log.i(UTAG,file_username );
+				saved = context.getSharedPreferences(file_username, MODE_PRIVATE);
+				String recording_completed = saved.getString(file.getName()+"completed","0");
+				if(recording_completed.equals("0")){
+					Log.i(UTAG, file_username+" is still recording this file.");
+					continue;
 				}
-				input.close();
-				ds.writeBytes(end);
-				ds.writeBytes(hyphens + boundary + hyphens + end);
-				ds.flush();
-				Log.i(UTAG, up_conn.getResponseCode() + "=======");
-				Log.i(UTAG + "end upload", file.getName());
-				// delete uploaded recordings
-				file.delete();
-				up_conn.getInputStream().close();
-				Log.i(UTAG + "file deleted", file.getName());
+				String upload_recording_url = saved.getString(file.getName(),"");
+				// Log.i(UTAG,upload_url );
+				String[] contents = sh.uploadUrlHelper(upload_recording_url);
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost(ServerUrl.audio_upload_url);
+				MultipartEntity multipartEntity = new MultipartEntity();
+				//upload with http
+				try {
+					multipartEntity.addPart("contextid",
+							new StringBody(contents[0]));
+					multipartEntity.addPart("component",
+							new StringBody(contents[1]));
+					multipartEntity
+							.addPart("filearea", new StringBody(contents[2]));
+					multipartEntity
+							.addPart("filename", new StringBody(contents[3]));
+					
+					String star = saved.getString(file.getName()+"star","none");
+					multipartEntity
+				    		.addPart("star", new StringBody(star));
+					
+					String mode = saved.getString(file.getName()+"mode","none");
+					multipartEntity
+				    		.addPart("mode", new StringBody(mode));
+					multipartEntity.addPart("file", new FileBody(file));
+
+					post.setEntity(multipartEntity);
+					HttpResponse resp = client.execute(post);
+					Log.i(UTAG, upload_recording_url + ": " + resp.getStatusLine()
+							+ "");
+					
+					moveFile(file.getName());
+					// if (resp.getStatusLine().getStatusCode() == 200) {
+					// Log.i("File Upload","Success");
+					file.delete();
+					// }
+
+				} catch (Exception e) {
+					Log.e(UTAG, "Error" + e.getMessage());
+					e.printStackTrace();
+					app.isUploading = false;
+				}
 			}
-			uploadDone = true;
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			app.isUploading = false;
 		}
 	}
-	
-	*/
 
 	private void moveFile(String filename) {
 		// TODO Auto-generated method stub
